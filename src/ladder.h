826ec795249821d020705ed2db56dcc2e83f56a8
Yogesh Hegde <yogesh@iotiot.in>
2020-12-23 18:22:26 +0530

    feat: Initial commit
    Signed-off-by: Yogesh Hegde <yogesh@iotiot.in>

---
+++ src/ladder.h
@@ -0,0 +1,77 @@
+#include <pthread.h>
+#include <stdint.h>
+
+//Internal buffers for I/O and memory. These buffers are defined in the
+//auto-generated glueVars.cpp file
+#define BUFFER_SIZE		1024
+/*********************/
+/*  IEC Types defs   */
+/*********************/
+
+typedef uint8_t  IEC_BOOL;
+
+typedef int8_t    IEC_SINT;
+typedef int16_t   IEC_INT;
+typedef int32_t   IEC_DINT;
+typedef int64_t   IEC_LINT;
+
+typedef uint8_t    IEC_USINT;
+typedef uint16_t   IEC_UINT;
+typedef uint32_t   IEC_UDINT;
+typedef uint64_t   IEC_ULINT;
+
+typedef uint8_t    IEC_BYTE;
+typedef uint16_t   IEC_WORD;
+typedef uint32_t   IEC_DWORD;
+typedef uint64_t   IEC_LWORD;
+
+typedef float    IEC_REAL;
+typedef double   IEC_LREAL;
+
+//Booleans
+extern IEC_BOOL *bool_input[BUFFER_SIZE][8];
+extern IEC_BOOL *bool_output[BUFFER_SIZE][8];
+
+//Bytes
+extern IEC_BYTE *byte_input[BUFFER_SIZE];
+extern IEC_BYTE *byte_output[BUFFER_SIZE];
+
+//Analog I/O
+extern IEC_UINT *int_input[BUFFER_SIZE];
+extern IEC_UINT *int_output[BUFFER_SIZE];
+
+//Memory
+extern IEC_UINT *int_memory[BUFFER_SIZE];
+extern IEC_DINT *dint_memory[BUFFER_SIZE];
+extern IEC_LINT *lint_memory[BUFFER_SIZE];
+
+//Special Functions
+extern IEC_LINT *special_functions[BUFFER_SIZE];
+
+//Common task timer
+extern unsigned long long common_ticktime__;
+
+//----------------------------------------------------------------------
+//FUNCTION PROTOTYPES
+//----------------------------------------------------------------------
+
+//MatIEC Compiler
+void config_run__(unsigned long tick);
+void config_init__(void);
+
+//glueVars.cpp
+void glueVars();
+void updateTime();
+
+//hardware_layer.cpp
+void initializeHardware();
+void finalizeHardware();
+void updateBuffersIn();
+void updateBuffersOut();
+
+//main.cpp
+void sleep_until(struct timespec *ts, int delay);
+void sleepms(int milliseconds);
+bool pinNotPresent(int *ignored_vector, int vector_size, int pinNumber);
+extern uint8_t run_openplc;
+void handleSpecialFunctions();
---
+++ src/main.cpp
@@ -0,0 +1,215 @@
+//-----------------------------------------------------------------------------
+// Copyright 2018 Thiago Alves
+// This file is part of the OpenPLC Software Stack.
+//
+// OpenPLC is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPLC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPLC.  If not, see <http://www.gnu.org/licenses/>.
+//------
+//
+// This is the main file for the OpenPLC. It contains the initialization
+// procedures for the hardware, network and the main loop
+// Thiago Alves, Jun 2018
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include "iec_types.h"
+#include "ladder.h"
+
+#define OPLC_CYCLE          50000000
+
+extern int opterr;
+//extern int common_ticktime__;
+IEC_BOOL __DEBUG;
+
+IEC_LINT cycle_counter = 0;
+
+unsigned long __tick = 0;
+pthread_mutex_t bufferLock; //mutex for the internal buffers
+//pthread_mutex_t logLock; //mutex for the internal log
+uint8_t run_openplc = 1; //Variable to control OpenPLC Runtime execution
+
+//-----------------------------------------------------------------------------
+// Helper function - Makes the running thread sleep for the ammount of time
+// in milliseconds
+//-----------------------------------------------------------------------------
+void sleep_until(struct timespec *ts, int delay)
+{
+    ts->tv_nsec += delay;
+    if(ts->tv_nsec >= 1000*1000*1000)
+    {
+        ts->tv_nsec -= 1000*1000*1000;
+        ts->tv_sec++;
+    }
+    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, ts,  NULL);
+}
+
+//-----------------------------------------------------------------------------
+// Helper function - Makes the running thread sleep for the ammount of time
+// in milliseconds
+//-----------------------------------------------------------------------------
+void sleepms(int milliseconds)
+{
+	struct timespec ts;
+	ts.tv_sec = milliseconds / 1000;
+	ts.tv_nsec = (milliseconds % 1000) * 1000000;
+	nanosleep(&ts, NULL);
+}
+
+//-----------------------------------------------------------------------------
+// Verify if pin is present in one of the ignored vectors
+//-----------------------------------------------------------------------------
+bool pinNotPresent(int *ignored_vector, int vector_size, int pinNumber)
+{
+    for (int i = 0; i < vector_size; i++)
+    {
+        if (ignored_vector[i] == pinNumber)
+            return false;
+    }
+    
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+// Disable all outputs
+//-----------------------------------------------------------------------------
+void disableOutputs()
+{
+    //Disable digital outputs
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (bool_output[i][j] != NULL) *bool_output[i][j] = 0;
+        }
+    }
+    
+    //Disable byte outputs
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        if (byte_output[i] != NULL) *byte_output[i] = 0;
+    }
+    
+    //Disable analog outputs
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        if (int_output[i] != NULL) *int_output[i] = 0;
+    }
+}
+
+//-----------------------------------------------------------------------------
+// Special Functions
+//-----------------------------------------------------------------------------
+void handleSpecialFunctions()
+{
+    //current time [%ML1024]
+    struct tm *current_time;
+    time_t rawtime;
+    
+    time(&rawtime);
+    // store the UTC clock in [%ML1027]
+    if (special_functions[3] != NULL) *special_functions[3] = rawtime;
+
+    current_time = localtime(&rawtime);
+    
+    rawtime = rawtime - timezone;
+    if (current_time->tm_isdst > 0) rawtime = rawtime + 3600;
+        
+    if (special_functions[0] != NULL) *special_functions[0] = rawtime;
+    
+    //number of cycles [%ML1025]
+    cycle_counter++;
+    if (special_functions[1] != NULL) *special_functions[1] = cycle_counter;
+    
+}
+
+int main(int argc,char **argv)
+{
+    //======================================================
+    //                 PLC INITIALIZATION
+    //======================================================
+    tzset();
+    time(&start_time);
+    config_init__();
+
+    //======================================================
+    //              HARDWARE INITIALIZATION
+    //======================================================
+    initializeHardware();
+    updateBuffersIn();
+    updateBuffersOut();
+
+    //======================================================
+    //              REAL-TIME INITIALIZATION
+    //======================================================
+    // Set our thread to real time priority
+    struct sched_param sp;
+    sp.sched_priority = 30;
+    printf("Setting main thread priority to RT\n");
+    if(pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp))
+    {
+        printf("WARNING: Failed to set main thread to real-time priority\n");
+    }
+
+    // Lock memory to ensure no swapping is done.
+    printf("Locking main thread memory\n");
+    if(mlockall(MCL_FUTURE|MCL_CURRENT))
+    {
+        printf("WARNING: Failed to lock memory\n");
+    }
+
+	//gets the starting point for the clock
+	printf("Getting current time\n");
+	struct timespec timer_start;
+	clock_gettime(CLOCK_MONOTONIC, &timer_start);
+
+	//======================================================
+	//                    MAIN LOOP
+	//======================================================
+	while(run_openplc)
+	{
+		//make sure the buffer pointers are correct and
+		//attached to the user variables
+		glueVars();
+        
+		updateBuffersIn(); //read input image
+
+        handleSpecialFunctions();
+		config_run__(__tick++); // execute plc program logic
+
+		updateBuffersOut(); //write output image
+        
+		updateTime();
+
+		sleep_until(&timer_start, common_ticktime__);
+	}
+    
+    //======================================================
+	//             SHUTTING DOWN OPENPLC RUNTIME
+	//======================================================
+    //pthread_join(interactive_thread, NULL);
+    printf("Disabling outputs\n");
+    disableOutputs();
+    //updateCustomOut();
+    updateBuffersOut();
+	finalizeHardware();
+    printf("Shutting down OpenPLC Runtime...\n");
+    exit(0);
+}
